8923cff4f92e76ac4d5d66278f88a9a099ee9d1d
lucaskauaa <lk051015lk@gmail.com>
2025-06-13 20:35:19 -0300

    Criando projeto-09
    Algoritmo lê um número inteiro e verifica se ele é par ou ímpar

---
+++ projeto-09/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Insira um número inteiro: ");
+		int numero = sc.nextInt();
+		
+		if (numero % 2 == 0) {
+			System.out.println("PAR");
+		} else {
+			System.out.println("IMPAR");
+		}
+		
+		sc.close();
+	}
+
+}
