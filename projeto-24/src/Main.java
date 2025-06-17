7a01fcded9b8f1878d50579871a60fa3017117bd
lucaskauaa <lk051015lk@gmail.com>
2025-06-17 17:48:27 -0300

    Criando projeto-24
    Algoritmo que conta de 1 ate 5

---
+++ projeto-24/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int N = sc.nextInt();
+//		int numeros = 0;
+//		
+//		for (int i = 0; i < N; i++) {
+//			numeros += sc.nextInt();
+//		}
+//		
+//		System.out.println(numeros);
+//		
+//		sc.close();
+		
+		for (int i = 0; i <= 5; i++) {
+			System.out.println(i);
+		}
+		
+		for (int i = 5; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
